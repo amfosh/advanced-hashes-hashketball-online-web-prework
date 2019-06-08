2f42b02ea621ebf1342b2db99f5f4f3da6e95cd3
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-08 11:00:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -189,9 +189,4 @@ rebound
 end
 
 def most_points_scored
-  points = 0
-  game_hash.collect do |location, team_data|
-    team_data[:players].collect do |player_name, data|
-      if data[:points] > points
-        
-    if data[:points]
+  game_hash[:]
